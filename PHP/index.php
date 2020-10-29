64b413c008ad3c7c2c6d514030a79ca660e3b752
Gab Menacho <72589489+GabMenacho@users.noreply.github.com>
2020-10-29 09:52:26 -0300

    revisao bootstrap divs

---
+++ PHP/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset = "UTF-8">
+    <meta name "viewport" content="width = device-width, inicial scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="../CSS/estilo.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title> Página inicial </title>
+</head>
+<body>
+
+    <div class = "container-fluid">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 bg-dark text-white"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! Animi vero nesciunt recusandae porro magni cum deserunt ea, 
+            provident modi impedit id obcaecati.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! 
+            </div>
+
+            <div class="col-md-4 col-sm-6 bg-primary text-white"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! Animi vero nesciunt recusandae porro magni cum deserunt ea, 
+            provident modi impedit id obcaecati.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! 
+            </div>
+
+            <div class="col-md-4 col-sm-6 bg-success text-white"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! Animi vero nesciunt recusandae porro magni cum deserunt ea, 
+            provident modi impedit id obcaecati.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! 
+            </div>
+        </div>
+
+        <div class = "container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 bg-dark text-white"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! Animi vero nesciunt recusandae porro magni cum deserunt ea, 
+            provident modi impedit id obcaecati.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! 
+            </div>
+
+            <div class="col-md-4 col-sm-6 bg-primary text-white"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! Animi vero nesciunt recusandae porro magni cum deserunt ea, 
+            provident modi impedit id obcaecati.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! 
+            </div>
+
+            <div class="col-md-4 col-sm-6 bg-success text-white"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! Animi vero nesciunt recusandae porro magni cum deserunt ea, 
+            provident modi impedit id obcaecati.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Qui aliquam nemo eum soluta beatae vitae tempora! 
+            </div>
+        </div>
+
+
+    </div>
+
+
+   <!-- <div class="container-login">
+        <form>
+            <div class="frupo-form">
+                <label for="email">E-mail: </label>
+                <input class="input-texto" type="email" name="email" id="email">
+            </div>
+
+            <div class="grupo-form">
+                <label for="senha">Senha: </label>
+                <input class="input-texto" type="password" name="senha" id="senha">
+            </div>
+            <input type="submit" value="Entrar">
+
+        </form>
+    </div> -->
+</body>
+</html>
